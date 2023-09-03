053a87dfe7f2edd6dfd141ea50b05724ec89b365
AAFAQ ZAIB <64011208+AAFAQ-ZAIB@users.noreply.github.com>
2023-09-03 11:53:35 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,179 @@
+
+function colorChange(){
+
+    var a = document.getElementById("book-printing");
+    var b = document.getElementById("book-ebook");
+
+    b.style.backgroundColor = "#bc3428";
+    a.style.backgroundColor = "#811d15";
+}
+
+function colorChange2(){
+
+    var a = document.getElementById("book-printing");
+    var b = document.getElementById("book-ebook");
+
+    a.style.backgroundColor = "#bc3428";
+    b.style.backgroundColor = "#811d15";
+}
+
+
+function display(){
+
+    var c = document.getElementById("Book-1-p");
+    var d = document.getElementById("Book-2-e");
+
+       d.style.visibility = "visible";
+       c.style.visibility = " hidden";
+}
+
+function display2(){
+
+    var c = document.getElementById("Book-1-p");
+    var d = document.getElementById("Book-2-e");
+
+       c.style.visibility = "visible";
+       d.style.visibility = " hidden";
+}
+
+
+
+
+function mobileMenu(){
+
+    var e = document.getElementById("myMenu");
+    
+    if (e.style.visibility === "hidden"){
+        document.getElementById("myMenu").style.visibility = "visible";
+        
+    }
+    else {
+        document.getElementById("myMenu").style.visibility = "hidden";
+        
+    }
+}
+
+
+function mobileMenu2(){
+
+    var z = document.getElementsByClassName("Req-main");
+    
+    if (z.style.display === "none"){
+        document.getElementsByClassName("Req-main").style.display = "block";
+        
+    }
+    else {
+        document.getElementsByClassName("Req-main").style.display = "none";
+        
+    }
+}
+
+
+
+function radioStyle(){
+
+    var f = document.getElementById("Radio-1");
+    var g = document.getElementById("Radio-2");
+
+    f.style.backgroundColor = "#d33a2c";
+    f.style.color = "#fff";
+    g.style.backgroundColor = "transparent";
+    g.style.color = "#4F4F4F";
+}
+
+function radioStyle2(){
+
+    var f = document.getElementById("Radio-1");
+    var g = document.getElementById("Radio-2");
+
+    g.style.backgroundColor = "#d33a2c";
+    g.style.color = "#fff";
+    f.style.backgroundColor = "transparent";
+    f.style.color = "#4F4F4F";
+}
+
+
+function radioStyle3(){
+
+    var h = document.getElementById("Radio-3");
+    var i = document.getElementById("Radio-4");
+    var j = document.getElementById("Radio-5");
+
+    h.style.backgroundColor = "#d33a2c";
+    h.style.color = "#fff";
+    i.style.backgroundColor = "transparent";
+    i.style.color = "#4F4F4F";
+    j.style.backgroundColor = "transparent";
+    j.style.color = "#4F4F4F";
+}
+
+function radioStyle4(){
+
+    var h = document.getElementById("Radio-3");
+    var i = document.getElementById("Radio-4");
+    var j = document.getElementById("Radio-5");
+
+    i.style.backgroundColor = "#d33a2c";
+    i.style.color = "#fff";
+    h.style.backgroundColor = "transparent";
+    h.style.color = "#4F4F4F";
+    j.style.backgroundColor = "transparent";
+    j.style.color = "#4F4F4F";
+}
+
+function radioStyle5(){
+
+    var h = document.getElementById("Radio-3");
+    var i = document.getElementById("Radio-4");
+    var j = document.getElementById("Radio-5");
+
+    j.style.backgroundColor = "#d33a2c";
+    j.style.color = "#fff";
+    h.style.backgroundColor = "transparent";
+    h.style.color = "#4F4F4F";
+    i.style.backgroundColor = "transparent";
+    i.style.color = "#4F4F4F";
+}
+
+
+function radioStyle6(){
+
+    var k = document.getElementById("Radio-6");
+    var l = document.getElementById("Radio-7");
+    var m = document.getElementById("Radio-8");
+
+    k.style.backgroundColor = "#d33a2c";
+    k.style.color = "#fff";
+    l.style.backgroundColor = "transparent";
+    l.style.color = "#4F4F4F";
+    m.style.backgroundColor = "transparent";
+    m.style.color = "#4F4F4F";
+}
+
+function radioStyle7(){
+
+    var k = document.getElementById("Radio-6");
+    var l = document.getElementById("Radio-7");
+    var m = document.getElementById("Radio-8");
+
+    l.style.backgroundColor = "#d33a2c";
+    l.style.color = "#fff";
+    k.style.backgroundColor = "transparent";
+    k.style.color = "#4F4F4F";
+    m.style.backgroundColor = "transparent";
+    m.style.color = "#4F4F4F";
+}
+
+function radioStyle8(){
+
+    var k = document.getElementById("Radio-6");
+    var l = document.getElementById("Radio-7");
+    var m = document.getElementById("Radio-8");
+
+    m.style.backgroundColor = "#d33a2c";
+    m.style.color = "#fff";
+    k.style.backgroundColor = "transparent";
+    k.style.color = "#4F4F4F";
+    l.style.backgroundColor = "transparent";
+    l.style.color = "#4F4F4F";
+}
